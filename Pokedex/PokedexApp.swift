d2fd1fe5eab70d2298f9dd737322a16709c305e5
Matteo Gentili <matteogentili20@gmail.com>
2023-05-13 15:43:37 +0200

    First commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Matteo Gentili on 13/05/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
